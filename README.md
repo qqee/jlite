3b8ffcef73dae581f17d0050464e32cbb8c5cd87
qqee <qq@qq.ee>
2016-04-25 17:18:27 +0800

    alpha

--- README.md
+++ README.md
@@ -1,2 +1,142 @@
-# jlite
-兼容IE6+，支持JSON、选择器、AJAX、COOKIE的jquery精简版替代品
+# JLite
+
+[http://www.qq.ee](https://www.qq.ee/post/qqee_youMIGHTnotNEEDuseJQUERY_toobig_want_smaller_alternative.html)
+
+###jquery太大了怎么替代？你可能并不需要jquery
+---------------------------
+#####如果你使用jquery只用到很少几个功能如选择器、取值赋值、遍历等，可以试试[JLite]，兼容jquery语法却只有4KB，你不需要引用额外js文件，复制原生代码到自己的js文件中即可替换jquery。
+---------------------------
+#####JLite是原生javascript编写的超薄封装。
+#####支持 选择器、JSON、AJAX、COOKIE。
+#####兼容 IE6+
+---------------------------
+```javascript
+//onload
+$(function(){alert("ok")});
+
+//选择器，返回 Element 或 Element数组
+//仅支持单一名称，不支持多条件/子孙选择
+$("#MYID") //返回单个对象
+$(".MYCLASS") //返回数组
+$("div") //返回数组
+
+//选择器的方法、Element对象的方法
+var a=$("#MYID");alert(a.html());
+$("#MYID").html() //不支持数组
+$("#MYID").text() //不支持数组
+$("#MYID").next() //邻居元素，不支持数组
+$("#MYID").before("<a href="#">IM_NEW_LINK</a>") //加入内容，不支持数组
+$("#MYID").after("<a href="#">IM_NEW_LINK</a>") //加入内容，不支持数组
+$("#MYID").attr("DATA","value") //value参数忽略时为取值，否则为赋值，不支持数组
+$(".MYCLASS").addClass("ABC") //支持数组
+$(".MYCLASS").removeClass("ABC") //支持数组
+$(".MYCLASS").toggleClass("ABC") //支持数组
+$(".MYCLASS").each(function(k,v){...}) //选择器返回数组时内置遍历方法
+
+//遍历
+$.each($(".MYCLASS"),function(k,v){...}) //通过外部函数遍历
+//$.each可遍历数组和对象
+var a=[1,2,3];
+var a={a:1,b:2,c:3};
+$.each(a,function(k,v){
+	alert(k+"===>"+v);
+});
+
+//返回数组或对象的长度，返回整数0~n
+$.len(a)
+
+//浏览器是否为ie，布尔值，非函数
+$.ie
+
+//返回10位长度时间戳(毫秒)
+$.ms()
+
+//返回13位长度时间戳(微秒)
+$.us()
+
+//返回时间字符如2015-01-10 01:24:55
+//a参为10位长度时间戳，忽略则取当前时间
+$.now(a)
+
+//判断字符是否为数字(整数或浮点数)，返回布尔值
+$.isnumber(s)
+
+
+//json操作
+//a参为字符返回解析后的对象
+//a参为对象返回编码的json字符
+var a={9999:"中文.a?",x:10,y:15,z:{z1:1,z2:222},arr:[{"a":1,"b":2},{"c":3,"d":4}]};
+var b=$.json(a)
+alert(b)
+var a='{"9999":"\u6d93\ue15f\u6783\u002e\u005ca\u003f","x":10,"y":15,"z":{"z1":1,"z2":222},"arr":{"0":{"a":1,"b":2},"1":{"c":3,"d":4}}}';
+var b=$.json(a)
+alert(b.z.z2)
+
+
+//cookie操作
+//写入cookie，[键，值，参数(若忽略则过期时间为会话结束,path为/,secure为false)]
+//expires的单位是天
+$.cookie("www.qq.ee",$.now(),{"expires":7,"path":"/","secure":0});
+alert($.cookie()); //取回全部cookie如[a=1;b=2;c=3]
+alert($.cookie("www.qq.ee")); //取回对应键的值，不存在时返回空字符""
+
+
+//ajax操作
+$.ajax({
+	type:"GET",
+	url:"http://ip.taobao.com/service/getIpInfo.php?ip=8.8.8.8",
+	url:"http://freeapi.ipip.net/8.8.8.8",
+	dataType:"json",//json或空
+	async:0,//异步，默认为1==true
+	timeout:5000,//毫秒，忽略则不判定超时
+	//data:"a=1&b=2",//仅POST时有效
+	error:function(){
+		alert("Error!");
+	},
+	success:function(s){
+		alert("Ok:\n"+s);
+		$.each(s,function(k,v){
+			alert(k+"===>"+v);
+		});
+	}
+});
+
+//----------------------------------------------------------------
+//其他用法请直接使用原生javascript方法，举例如下:
+
+//事件绑定:
+$("#MYID").onclick=function(){}
+obj.onfocus=function(){}
+obj.onchange=function(){}
+window.onscroll=function(){}
+
+//修改css/style:
+$("#MYID").style.width="100px";
+obj.style.display="inline-block";
+obj.style.position='absolute';
+obj.style.opacity=1;
+
+//到顶部:
+document.body.scrollTop=0;
+
+//到网页底部:
+document.body.scrollTop=$("#bot").offsetTop;
+
+//修改src属性:
+obj.src="/test.html?t="+$.ms();
+
+//插入对象/代码:
+var u=document.createElement("style");
+u.innerHTML="div{color:red}";
+document.head.appendChild(u);
+
+//修改网页标题:
+document.title="www.qq.ee";
+
+
+```
+
+
+
+
+
---
+++ jlite.js
@@ -0,0 +1,256 @@
+/*
+copyright:
+[jlite.0.0.1]
+[http://www.qq.ee]
+[https://github.com/qqee/jlite]
+*/
++function(win){
+	var jlite=(function(){
+		var e=function(v){return e.prototype.f(v)};
+		return e.prototype.f=function(f,o){
+			var t=typeof f;
+			if("function"==t){
+				if(document.addEventListener){
+					document.addEventListener("DOMContentLoaded",function(){
+						document.removeEventListener("DOMContentLoaded",arguments.callee,!1);
+						f()
+					},!1);
+				}else if(document.attachEvent){
+					document.attachEvent("onreadystatechange",function(){
+						if(document.readyState==="complete"){
+							document.detachEvent("onreadystatechange",arguments.callee);
+							f()
+						}
+					});
+				}
+			}else if("string"==t){
+				var _=f.substr(0,1);
+				var z=f.substr(1);
+				var n=z.length;
+				var d=document;
+				if(_=="#"){
+					var o=d.getElementById(z);
+					jlite._b(o);
+					return o;
+				}else{
+					if(!o)o=d.body;
+					var all=o.getElementsByTagName((_==".")?("*"):(f)),arr=[];
+					for(var i=0;i<all.length;i++){
+						if(_!="."){
+							jlite._b(all[i]);
+							arr.push(all[i]);
+						}else{
+							var c=all[i].className;
+							if(c.length>=n){
+								var m=c.indexOf(z);
+								if(m>=0){
+									var g=c.split(" ");
+									for(var v in g){
+										if(g[v]==z){
+											jlite._b(all[i]);
+											arr.push(all[i]);
+										}
+									}
+								}
+							}
+						}
+					}
+					arr.each=function(f){
+						for(var i=0;i<this.length;i++)f(i,this[i]);
+					}
+					jlite._b(arr,1);
+					return arr;
+				}
+				
+			}
+		}
+	})();
+	jlite.ie=!-[1,];
+	jlite.each=function(o,f){
+		if("object"!=typeof(o))return;
+		if(o.length){
+			for(var i=0;i<o.length;i++)f(i,o[i]);
+			return;
+		}
+		for(var v in o)f(v,o[v]);
+	}
+	jlite.isnumber=function(s){
+		if(s)return !isNaN(s);
+		return false;
+	}
+	jlite.len=function(o){
+		if(!o)return 0;
+		var n=0;
+		for(var v in o)n++;
+		return n;
+	}
+	jlite.eu=function(str){
+		var a=[];
+		var pcs="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		for(var i=0;i<str.length;i++){
+			if(pcs.indexOf(str.substr(i,1))==-1)
+				a[i]="\\u"+("0000"+str.charCodeAt(i).toString(16)).slice(-4);
+			else a[i]=str.substr(i,1);
+		}
+		return a.join("");
+	}
+	jlite.json=function(o){
+		var _=typeof(o);
+		if(_=="string"){
+			var r;
+			try{
+				r=(new Function("return "+o))();
+			}catch(e){}
+			return r;
+		}else if(_=="object"){
+			var ff=function(p){
+				var arr=[];
+				for(var v in p){
+					var t=typeof(p[v]);
+					var s='"'+jlite.eu(v)+'":';
+					if(t=="object"){
+						s+=ff(p[v]);
+					}else{
+						if(t=="string")
+							s+='"'+jlite.eu(p[v])+'"';
+						else if(t=='number')
+							s+=p[v];
+					}
+					arr.push(s);
+				}
+				return "{"+arr.join(",")+"}";
+			}
+			return ff(o);
+		}
+	}
+	jlite.get=function(k,v){
+		var r=v;
+		var where=document.location.href.indexOf(k+"=");
+		if(where>0){
+			r=document.location.href.substr(where+k.length+1);
+			where=r.indexOf("&");
+			if(where>0)r=r.substr(0,where);
+		}
+		return decodeURIComponent(r);
+	}
+	jlite._s=function(us){
+		return(us?(new Date().getTime()):(parseInt((new Date().getTime())/1000)));
+	}
+	jlite.ms=function(){
+		return jlite._s();
+	}
+	jlite.us=function(){
+		return jlite._s(1);
+	}
+	jlite.now=function(unix){
+		unix||(unix=jlite.ms());
+		var date=new Date(unix*1000);
+		var s=date.getFullYear()+'-';
+		s+=(date.getMonth()+1<10?('0'+(date.getMonth()+1)):date.getMonth()+1)+'-';
+		s+=(date.getDate()<10?('0'+date.getDate()):date.getDate())+' ';
+		s+=(date.getHours()<10?('0'+date.getHours()):date.getHours())+':';
+		s+=(date.getMinutes()<10?('0'+date.getMinutes()):date.getMinutes())+':';
+		s+=(date.getSeconds()<10?('0'+date.getSeconds()):date.getSeconds());
+		return s;
+	}
+	jlite.cookie=function(k,v,r){
+		if(v){
+			var s=(r&&r.expires)?(";expires="+new Date(+new Date()+r.expires*864e5).toGMTString()):("");
+			s+=(r&&r.secure)?(";secure"):("");
+			s+=";path:"+((r&&r.path)?(r.path):("/"));
+			document.cookie=(k+"="+v+s);
+			return;
+		}
+		if(k){
+			var o=(new RegExp("(^| )"+k+"=([^;]*)(;|$)")).exec(document.cookie);
+			return(o?o[2]:"");
+		}
+		return document.cookie;
+	}
+	jlite.ajax=function(c){
+		if(!c.url)return false;
+		var r=(window.XMLHttpRequest)?(new XMLHttpRequest()):(new ActiveXObject("Microsoft.XMLHTTP"));
+		r.open((c.type||"GET"),c.url,((c.async==undefined)?(true):(c.async?true:false)));
+		r.onreadystatechange=function(){
+			if (this.readyState==4 && this.status==200){
+				(typeof(c.success)=="function")&&(c.success((c.dataType=="json")?(jlite.json(this.responseText)):(this.responseText)));
+			}
+		}
+		r.onerror=function(){if(c.error)c.error()};
+		r.send((c.data||""));
+		if(c.timeout)setTimeout(function(){r&&r.abort()},c.timeout);
+		return true;
+	}
+	jlite.html=function(v){
+		var _=this;
+		if(!v)return _.innerHTML;
+		_.innerHTML=v;
+	}
+	jlite.text=function(v){
+		if(!v)return this.textContent;
+		this.textContent=v;
+	}
+	jlite.addClass=function(s){
+		var _=this;
+		(_.length)?(jlite.each(_,function(k,v){
+			(v.classList)?(v.classList.add(s)):(v.className+=' '+s)
+		})):((_.classList)?(_.classList.add(s)):(_.className+=' '+s));
+	}
+	jlite.removeClass=function(s){
+		var _=this;
+		(_.length)?(jlite.each(_,function(k,v){
+			(v.classList)?(v.classList.remove(s)):(v.className=v.className.replace(new RegExp('(^|\\b)'+s.split(' ').join('|')+'(\\b|$)','gi'),' '))
+		})):((_.classList)?(_.classList.remove(s)):(_.className=_.className.replace(new RegExp('(^|\\b)'+s.split(' ').join('|')+'(\\b|$)','gi'),' ')));
+	}
+	jlite.toggleClass=function(s){
+		var _=this;
+		if(_.length){
+			jlite.each(_,function(k,v){
+				if(v.classList){
+					v.classList.toggle(s)
+				}else{
+					var a=v.className;v.removeClass(s);if(v.className==a)v.addClass(s)
+				}
+			})
+		}else{
+			if(_.classList){
+				_.classList.toggle(s)
+			}else{
+				var a=_.className;_.removeClass(s);if(_.className==a)_.addClass(s);
+			}
+		}
+	}
+	jlite.next=function(){
+		var o=this;
+		do{o=o.nextSibling;}while(o&&o.nodeType!==1);
+		jlite._b(o);
+		return o;
+	}
+	jlite.before=function(s){
+		this.insertAdjacentHTML('beforebegin',s);
+	}
+	jlite.after=function(s){
+		this.insertAdjacentHTML('afterend',s);
+	}
+	jlite.attr=function(s,v){
+		if(!v)return this.getAttribute(s);
+		this.setAttribute(s,v);
+	}
+	jlite._b=function(o,m){
+		if(m||jlite.ie){
+			o.addClass=jlite.addClass;
+			o.removeClass=jlite.removeClass;
+			o.toggleClass=jlite.toggleClass;
+			o.next=jlite.next;
+			o.before=jlite.before;
+			o.after=jlite.after;
+			o.attr=jlite.attr;
+			o.html=jlite.html;
+			o.text=jlite.text;
+		}
+	}
+	if(!jlite.ie){
+		jlite._b(HTMLElement.prototype,1);
+	}
+	win.$=win.jlite=jlite;
+}(window);
